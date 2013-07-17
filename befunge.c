4bc70c6cf6b9f8f8a26242c14a0ab94443e1e5a9
Michael Burge <michael.burge@rentrakmail.com>
2013-07-16 21:53:00 -0700

    Added Reverse, TurnRight, TurnLeft; added stubs and jump table entries for all Funge-98 instructions

--- befunge.c
+++ befunge.c
@@ -26,7 +26,7 @@ VectorStats XStats(char y)
 VectorStats YStats(uint8_t x) { return XStats(x); }
 
 bool isInBounds(uint8_t value, uint8_t start, uint8_t length) { return (value < start || value >= (start + length)); }
-
+ 
 bool isNetherZone(Index *idx)
 {
 	VectorStats xStats = XStats(idx->y), yStats = YStats(idx->x);
@@ -77,15 +77,80 @@ uint8_t *defaultGrid() { static uint8_t ret[] = { [0 ... 35] = ' ' }; return ret
 uint32_t *defaultColors() { static uint32_t ret[] = { [0 ... 35] = 0 }; return ret; }
 
 /* Opcodes */
+/* http://quadium.net/funge/spec98.html */
 typedef void (*Mutator)(InterpreterState*);
 
 void IrrecoverableError(InterpreterState *state) { state->errorFlag = 1; }
+void UnimplementedInstruction(InterpreterState *state) { state->errorFlag = 1; }
 void DoNothing(InterpreterState *state) { return; }
+// Direction Changing
 void GoNorth(InterpreterState *state) { state->delta.dx =  0; state->delta.dy = -1; }
 void GoSouth(InterpreterState *state) { state->delta.dx =  0; state->delta.dy = +1; }
 void GoEast(InterpreterState *state)  { state->delta.dx = +1; state->delta.dy =  0; }
 void GoWest(InterpreterState *state)  { state->delta.dx = -1; state->delta.dy =  0; }
+void GoHigh(InterpreterState *state) { IrrecoverableError(state); }
+void GoLow(InterpreterState *state) { IrrecoverableError(state); }
 void GoRandom(InterpreterState *state) { Mutator jmp[] = {&GoNorth, &GoSouth, &GoWest, &GoEast}; jmp[rand()%4](state); }
+void Reverse(InterpreterState *state) { state->delta.dx *= -1; state->delta.dy *= -1; }
+void TurnRight(InterpreterState *state) { uint8_t old_dx = state->delta.dx; state->delta.dx = -state->delta.dy; state->delta.dy = state->delta.dx; }
+void TurnLeft(InterpreterState *state) { Reverse(state), TurnRight(state), Reverse(state); }
+void AbsoluteDelta(InterpreterState *state) { UnimplementedInstruction(state); }
+// Flow Control
+void Trampoline(InterpreterState *state) { UnimplementedInstruction(state); }
+void Stop(InterpreterState *state) { UnimplementedInstruction(state); }
+void JumpOver(InterpreterState *state) { UnimplementedInstruction(state); }
+void JumpForward(InterpreterState *state) { UnimplementedInstruction(state); }
+void Quit(InterpreterState *state) { UnimplementedInstruction(state); }
+void Iterate(InterpreterState *state) { UnimplementedInstruction(state); }
+// Decision Making
+void LogicalNot(InterpreterState *state) { UnimplementedInstruction(state); }
+void GreaterThan(InterpreterState *state) { UnimplementedInstruction(state); }
+void EastWestIf(InterpreterState *state) { UnimplementedInstruction(state); }
+void NorthSouthIf(InterpreterState *state) { UnimplementedInstruction(state); }
+void HighLowIf(InterpreterState *state) { IrrecoverableError(state); }
+void Compare(InterpreterState *state) { UnimplementedInstruction(state); }
+// Integers
+void Push(InterpreterState *state) { UnimplementedInstruction(state); }
+void Add(InterpreterState *state) { UnimplementedInstruction(state); }
+void Multiply(InterpreterState *state) { UnimplementedInstruction(state); }
+void Subtract(InterpreterState *state) { UnimplementedInstruction(state); }
+void Divide(InterpreterState *state) { UnimplementedInstruction(state); }
+void Remainder(InterpreterState *state) { UnimplementedInstruction(state); }
+// Strings
+void ToggleStringmode(InterpreterState *state) { UnimplementedInstruction(state); }
+void FetchCharacter(InterpreterState *state) { UnimplementedInstruction(state); }
+void StoreCharacter(InterpreterState *state) { UnimplementedInstruction(state); }
+// Stack Manipulation
+void Pop(InterpreterState *state) { UnimplementedInstruction(state); }
+void Duplicate(InterpreterState *state) { UnimplementedInstruction(state); }
+void Swap(InterpreterState *state) { UnimplementedInstruction(state); }
+void ClearStack(InterpreterState *state) { UnimplementedInstruction(state); }
+// Stack Stack Manipulation
+void BeginBlock(InterpreterState *state) { UnimplementedInstruction(state); }
+void EndBlock(InterpreterState *state) { UnimplementedInstruction(state); }
+void StackUnderStack(InterpreterState *state) { UnimplementedInstruction(state); }
+// Funge-Space Storage
+void Get(InterpreterState *state) { UnimplementedInstruction(state); }
+void Put(InterpreterState *state) { UnimplementedInstruction(state); }
+// Standard Input/Output
+void OutputDecimal(InterpreterState *state) { UnimplementedInstruction(state); }
+void OutputCharacter(InterpreterState *state) { UnimplementedInstruction(state); }
+void InputDecimal(InterpreterState *state) { UnimplementedInstruction(state); }
+void InputCharacter(InterpreterState *state) { UnimplementedInstruction(state); }
+// File Input/Output
+void InputFile(InterpreterState *state) { UnimplementedInstruction(state); }
+void OutputFile(InterpreterState *state) { UnimplementedInstruction(state); }
+// System
+void Execute(InterpreterState *state) { UnimplementedInstruction(state); }
+void GetSysInfo(InterpreterState *state) { UnimplementedInstruction(state); }
+// Fingerprints
+void CustomInstruction(InterpreterState *state) { UnimplementedInstruction(state); }
+void LoadSemantics(InterpreterState *state) { UnimplementedInstruction(state); }
+void UnloadSemantics(InterpreterState *state) { UnimplementedInstruction(state); }
+// Concurrent Funge-98
+void Split(InterpreterState *state) { UnimplementedInstruction(state); }
+
+
 
 
 void applyDeltaToIp(InterpreterState *state)
@@ -105,11 +170,61 @@ void runInterpreterStep(InterpreterState *state)
 	Mutator jmp[] = {
 		[0 ... 255] = &IrrecoverableError,
 		[' '] = &DoNothing,
-		['^'] = &GoNorth,
-		['v'] = &GoSouth,
+		['!'] = &LogicalNot,
+		['"'] = &ToggleStringmode,
+		['#'] = &Trampoline,
+		['$'] = &Pop,
+		['%'] = &Remainder,
+		['&'] = &InputDecimal,
+		['\'']= &FetchCharacter,
+		['('] = &LoadSemantics,
+		[')'] = &UnloadSemantics,
+		['*'] = &Multiply,
+		['+'] = &Add,
+		[','] = &OutputCharacter,
+		['-'] = &Subtract,
+		['.'] = &OutputDecimal,
+		['/'] = &Divide,
+		['0' ... '9'] = &Push,
+		['a' ... 'f'] = &Push,
+		[':'] = &Duplicate,
+		[';'] = &JumpOver,
 		['<'] = &GoWest,
+		['='] = &Execute,
 		['>'] = &GoEast,
 		['?'] = &GoRandom,
+		['@'] = &Stop,
+		['A' ... 'Z'] = &CustomInstruction,
+		['['] = &TurnLeft,
+		['\\'] = &Swap,
+		[']'] = &TurnRight,
+		['^'] = &GoNorth,
+		['_'] = &EastWestIf,
+		['`'] = &GreaterThan,
+		['g'] = &Get,
+		['h'] = &GoHigh,
+		['i'] = &InputFile,
+		['j'] = &JumpForward,
+		['k'] = &Iterate,
+		['l'] = &GoLow,
+		['m'] = &HighLowIf,
+		['n'] = &ClearStack,
+		['o'] = &OutputFile,
+		['p'] = &Put,
+		['q'] = &Quit,
+		['r'] = &Reverse,
+		['s'] = &StoreCharacter,
+		['t'] = &Split,
+		['u'] = &StackUnderStack,
+		['v'] = &GoSouth,
+		['w'] = &Compare,
+		['x'] = &AbsoluteDelta,
+		['y'] = &GetSysInfo,
+		['z'] = &DoNothing,
+		['{'] = &BeginBlock,
+		['|'] = &NorthSouthIf,
+		['}'] = &EndBlock,
+		['~'] = &InputCharacter,
 	};
 
 	auto offset = indexToOffset(state->ip);
